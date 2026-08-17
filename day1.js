bb2609cf5409698161b0aa6ec95edf904ebf5e5b
m77204268-boop <m77204268@gmail.com>
2026-08-17 07:04:33 +0500

    Added day 1 JS practice

---
+++ day1.js
@@ -0,0 +1,13 @@
+// Day 1 - Javascript Practice
+
+let name = "Muhammad Haris";
+let age = 19;
+
+console.log("My name is " + name);
+console.log("My age is " + age);
+
+if (age >= 18) {
+    console.log("I am an adult.");
+} else {
+    console.log("I am a minor.");
+}
