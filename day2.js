fc4f4dd3c057077a24ff9c16447eb4da4c47ec17
m77204268-boop <m77204268@gmail.com>
2026-08-20 10:10:50 +0500

    Day 2: comparison operators, increment/decrement practice

---
+++ day2.js
@@ -0,0 +1,93 @@
+// console.log("Hello, World!");
+// console.log("Apna College");
+
+// let a = 10;
+// let b = 20;
+// console.log("sum is =", a + b);
+
+let pencilprice = 10;
+let penprice = 20;
+// aik ya tareeqa
+// console.log(`The Total Price is = ${pencilprice + penprice} Rupees.`);   
+
+//dosra tareeqa
+// console.log("The Tootal price is = " , (pencilprice + penprice) , " Rupees.");
+
+//teesra tareeqa
+// let output = "The Total price is = " + (pencilprice + penprice) + " Rupees.";
+// console.log(output)
+
+//practice
+let item = "Rice";
+let Quantity = 5;
+let price = 150;
+console.log (`The total price is = ${Quantity * price} Rupees.`);
+
+let studentName = "Ali";
+let marksobtained = 450;
+let totalmarks = 500;
+console.log (`The percentage of ${studentName} is = ${(marksobtained / totalmarks) * 100} %`);
+
+let celcius = 37;
+let fahrenheit = (celcius * 9/5) + 32;
+console.log (`The temperature in Fahrenheit is = ${fahrenheit} °F`);
+
+let movieName = "Avengers";
+let ticketnumber = "A12";
+let ticketprice = 500;
+// console.log(`The seat was booked for avengers movie = ${ticketprice} Rupees`);
+console.log(`The seat ${ticketnumber} was booked for ${movieName} movie = ${ticketprice} Rupees`);
+
+let item1price = 200;
+let item1qty = 2;
+let item2price = 150;
+let item2qty = 3;
+console.log(`Your total bill is = ${(item1price*item1qty)+(item2price*item2qty)} Rupees`);
+
+let principle = 1000;
+let rate = 5;
+let time = 2;
+console.log (`principle : ${principle}
+    rate : ${rate}
+    time : ${time}
+    Intrest : ${(principle*rate*time)/100}`);
+
+//operators
+let x = 3;
+let y = 5;
+console.log(x-y);    
+
+let score = 50;
+ score += 5;
+ score -= 10;
+ score = 50;
+ score *= 10;
+console.log(score);
+
+let visits = 0;
+visits++;
+visits++;
+visits++;
+visits--;
+console.log(visits);
+
+//Comparison operator
+// et a = 8;
+// let b = 9;
+// console.log(a > b );
+// console.log(a<b);
+// console.log(a>=8);l
+
+let a = 10;
+let b = "10";
+
+console.log(a == b);    // guess karo: true ya false?
+console.log(a === b);   // guess karo: true ya false?
+
+
+let stock = 25
+console.log(stock >10);
+console.log(stock ===25);
+console.log("cat" < "dog");
+
+console.log("banana" < "apple")
